51da23552cd02515c756d51d5cc14bd430bda035
yatsoon <kyatsoon1993@yahoo.com.sg>
2022-02-03 15:15:19 +0800

    made some more minor changes to the quiz

--- quiz.js
+++ quiz.js
@@ -90,7 +90,7 @@ next_btn.onclick = ()=>{
 
 // getting questions and options from array
 function showQuetions(index){
-    const que_text = document.querySelector(".que_text");
+    const text = document.querySelector(".text");
 
     //creating a new span and div tag for question and option and passing the value using array index
     let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
@@ -98,7 +98,7 @@ function showQuetions(index){
     + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
     + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
     + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
-    que_text.innerHTML = que_tag; //adding new span tag inside que_tag
+    text.innerHTML = que_tag; //adding new span tag inside que_tag
     option_list.innerHTML = option_tag; //adding new div tag inside option_tag
     
     const option = option_list.querySelectorAll(".option");
